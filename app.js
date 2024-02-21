589fe29332d92929897ab0d15208faf2af313fd4
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-02-21 12:57:59 +0100

    All the buttons are connected to app.js and now the balance is displayed.

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 // Access to the button #accountNumber
 const accountInput = document.querySelector('#accountNumber');
 // Access to the button #checkBalance
-const checkBalanceBtn = document.querySelector('#checkBalance');
+const checkBalanceButton = document.querySelector('#checkBalance');
 // Access to the div #balance
-const balanceDiv = document.querySelector('#balance');
+const displayBalance = document.querySelector('#balance');
+
+
 // To use Ganache, create a variable and set it to endpoint 'http://127.0.0.1:7545'
 const rpc = new Web3('http://127.0.0.1:7545');
+
+// Set a let variable for account
+let account;
 
 function initApp () {
 console.log(rpc);
 }
 
+async function checkBalance () {
+    account = accountInput.value;
+    // Get the balance of the account. Async! We need to await. Rpc from web3.js is an Eth library
+    const balance = await rpc.eth.getBalance(account);
+
+    // Display the balance
+    document.querySelector('#balance').innerHTML = balance;
+}
+
 document.addEventListener('DOMContentLoaded', initApp)
+// Connect an eventListener to the button #checkBalance
+checkBalanceButton.addEventListener('click', checkBalance);
