6349b97d24765f9a90239e623f2a353b3e863e4d
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-02-21 14:19:00 +0100

    Add JS to make the transfer button work. It works.

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const accountInput = document.querySelector('#accountNumber');
 const checkBalanceButton = document.querySelector('#checkBalance');
 // Access to the div #balance
 const displayBalance = document.querySelector('#balance');
+// Access to the button #toAccountNumber
+const toAccountInput = document.querySelector('#toAccountNumber');
+// Access to the button #amount
+const valueInput = document.querySelector('#amount');
+// Access to the button #transfer
+const sendButton = document.querySelector('#sendTx');
 
 
 // To use Ganache, create a variable and set it to endpoint 'http://127.0.0.1:7545'
@@ -25,6 +31,31 @@ async function checkBalance () {
 
 }
 
+async function sendTransaction () {
+    // Fetch the account to sent the transaction
+    const toAddress = toAccountInput.value;
+    // Fetch the value (calling the var amount so it defers from value)
+    const amount = valueInput.value;
+    // Send the transaction
+    try {
+        // Create the object to be sent. 
+        // Documentation: https://web3js.readthedocs.io/en/v1.2.11/web3-eth.html#sendtransaction
+        const trx = await rpc.eth.sendTransaction(
+            // The transaction object:
+            {
+            from: account,
+            to: toAddress,
+            // Convert the amount to ether
+            value: rpc.utils.toWei(amount, 'ether'),
+            // gas: 21000
+            
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', initApp)
 // Connect an eventListener to the button #checkBalance
 checkBalanceButton.addEventListener('click', checkBalance);
+sendButton.addEventListener('click', sendTransaction);
